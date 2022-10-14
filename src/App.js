01b5dac8604749134576d87a11b89f48d2df17c9
Nitha <nitha.spst@gmail.com>
2022-10-14 22:47:44 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. MAIN
+          Edit <code>src/App.js</code> and save to reload. MAIN22
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React MAIN
+          Learn React MAIN22
         </a>
       </header>
     </div>
