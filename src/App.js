0526903614b49ab3c451934648eb5fbad76c1e28
Nitha <nitha.spst@gmail.com>
2022-10-15 03:21:09 +0700

    fix firebase config 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. FIX
+          Edit <code>src/App.js</code> and save to reload. FIX2
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React FIX
+          Learn React FIX2
         </a>
       </header>
     </div>
